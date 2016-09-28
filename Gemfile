4718411b1021dc410108b65096028ddd6de5973a
RyanCrocombe <ryancrocombe@gmail.com>
2016-09-28 22:40:29 +1300

    Design almost finished - images and content to finalise

--- Gemfile
+++ Gemfile
@@ -22,6 +22,11 @@ group :test do
 	gem 'capybara'
 end
 
+group :production do
+	gem 'pg'
+	gem 'rails_12factor'
+end
+
 group :development do
   gem 'web-console', '~> 2.0'
   gem 'spring'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  get 'pages/trans'
+
+  get 'pages/about'
+
+  get 'pages/faq'
+
+  get 'pages/contact'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
